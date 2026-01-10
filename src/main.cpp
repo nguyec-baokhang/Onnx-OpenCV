2d37c0acf02fc916916671cdffe7495bc1cf5bfe
nguyec-baokhang <nguyecb@mail.uc.edu>
2026-01-09 22:56:13 -0500

    Full code but weird objs

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <onnxruntime_cxx_api.h>
 #include <opencv2/opencv.hpp>
 
+struct YoloBoundingBox{
+  cv::Rect bounding_box;
+  double confidence;
+  int class_id;
+};
+
 Ort::Session LoadYoloModel(const Ort::Env &env, const std::string &modelPath){
   Ort::SessionOptions yoloSessionOption;
   Ort::Session yoloModelSession(env,modelPath.c_str(),yoloSessionOption);
@@ -34,6 +40,13 @@ cv::Mat LetterBox(cv::Mat img, cv::Size new_size){
   return padded_img;
 }
 
+void ClipBox(cv::Rect &box, const cv::Size &shape){
+  box.x = std::max(0,std::min(box.x,shape.width));
+  box.y = std::max(0,std::min(box.y,shape.height));
+  box.width = std::max(0, std::min(box.width, shape.width - box.x));
+  box.height = std::max(0, std::min(box.height, shape.height - box.y));
+}
+
 cv::Mat LoadImage(const std::string &imagePath){
   cv::Mat image = cv::imread(imagePath);
   cv::Mat padded_image;
@@ -49,13 +62,126 @@ cv::Mat LoadImage(const std::string &imagePath){
   );
 }
 
+Ort::Value BlobToOnnxTensor(const cv::Mat &blob){
+  // Create tensor 
+  std::vector<int64_t> tensor_shape = {1,3,640,640};
+  int64_t input_size = 1 * 3 * 640 * 640;
+  Ort::MemoryInfo memory_info = Ort::MemoryInfo::CreateCpu(OrtArenaAllocator, OrtMemTypeDefault);
+  Ort::Value tensor = Ort::Value::CreateTensor(
+    memory_info,
+    (float*) (blob.data), // C-style casting
+    input_size,
+    tensor_shape.data(),
+    tensor_shape.size()
+  );
+  return tensor;
+}
+
+cv::Mat getYoloBox(std::vector<Ort::Value> &output){
+  std::vector<int64_t> output_shape = output[0].GetTensorTypeAndShapeInfo().GetShape();
+  return cv::Mat(
+    static_cast<int>(output_shape[2]), // rows (e.g., 8400)
+    static_cast<int>(output_shape[1]), // cols (e.g., 84)
+    CV_32F,
+    output[0].GetTensorMutableData<float>()
+).t();
+}
+
+void ScaleYoloBox(YoloBoundingBox &box, const cv::Size &original_shape){
+  cv::Size yolo_shape(640,640);
+  float scale_ratio = std::min(
+    static_cast<float>(yolo_shape.height / original_shape.height),
+    static_cast<float>(yolo_shape.width / original_shape.width)
+  );
+  float letterbox_padding_x = (yolo_shape.width - original_shape.width * scale_ratio) / 2;
+  float letterbox_padding_y = (yolo_shape.height - original_shape.height * scale_ratio) / 2;
+  box.bounding_box.x -= letterbox_padding_x; 
+  box.bounding_box.y -= letterbox_padding_y; 
+  box.bounding_box.width /= scale_ratio;
+  box.bounding_box.height /= scale_ratio;
+  ClipBox(box.bounding_box, original_shape);
+}
+
+std::vector<YoloBoundingBox> ProcessYoloOutputs(const cv::Mat &raw_boxes, const cv::Size &original_shape){
+  int num_classes{80};
+  int data_width = num_classes + 4; 
+  float *box_data = reinterpret_cast<float *>(raw_boxes.data);
+  std::vector<YoloBoundingBox> processed_boxes;
+
+  for(int row = 0; row < raw_boxes.rows; row++){
+    float center_x = box_data[0];
+    float center_y = box_data[1];
+    float width = box_data[2];
+    float height = box_data[3];
+    float x = std::max(center_x - 0.5 * width, 0.0);
+    float y = std::max(center_y - 0.5 * height, 0.0);
+    cv::Rect bounding_box(static_cast<int>(x), static_cast<int>(y), static_cast<int>(width), static_cast<int>(height));
+    cv::Mat class_scores = cv::Mat(1, num_classes, CV_32FC1, box_data + 4); 
+    cv::Point class_id_tmp;
+    double confidence; 
+    cv::minMaxLoc(class_scores, nullptr, &confidence, nullptr, &class_id_tmp); 
+    int class_id = class_id_tmp.x;
+  
+    if(confidence > 0.25){
+      YoloBoundingBox parsed_box{bounding_box,confidence,class_id};
+      processed_boxes.push_back(parsed_box);
+    }
+    box_data += data_width;
+  }
+  // Step 3: Scale boxes
+  float conf_thres_hold{0.25};
+  float iou{0.7};
+  std::vector<cv::Rect>boxes;
+  std::vector<float>confidences;
+  std::vector<int>filtered_indices;
+  for(size_t i = 0; i < processed_boxes.size(); i++){
+    auto &box = processed_boxes[i];
+    ScaleYoloBox(box,original_shape);
+    // Step 4: Non-max suppression 
+    boxes.push_back(box.bounding_box);
+    confidences.push_back(static_cast<float>(box.confidence));
+  }
+  cv::dnn::NMSBoxes(boxes, confidences, 0.25, 0.70, filtered_indices);
+  std::vector<YoloBoundingBox> filtered_boxes;
+  for(size_t i = 0; i < filtered_indices.size(); i++){
+    filtered_boxes.push_back(processed_boxes[filtered_indices[i]]);
+  }
+
+  return filtered_boxes;
+}
+
 int main(){
   const Ort::Env env(ORT_LOGGING_LEVEL_ERROR,"YOLO");
   const std::string modelPath = "../models/yolov8n.onnx";
   const std::string imagePath = "../images/image1.jpg";
 
   Ort::Session yolo_model_session = LoadYoloModel(env,modelPath);
+  cv::Mat image = cv::imread(imagePath);  
   cv::Mat blob = LoadImage(imagePath);
-  // cv::imshow("Image Window",blob);
+  Ort::Value input_tensor = BlobToOnnxTensor(blob);
+
+  // Inferencing 
+  Ort::AllocatorWithDefaultOptions allocator;
+  const std::string input_name = yolo_model_session.GetInputNameAllocated(0,allocator).get();
+  const std::string output_name = yolo_model_session.GetOutputNameAllocated(0,allocator).get();
+  const char* input_names[] = {input_name.c_str()};
+  const char* output_names[] = {output_name.c_str()};
+
+  std::vector<Ort::Value> output = yolo_model_session.Run(
+    Ort::RunOptions{nullptr},
+    input_names,
+    &input_tensor,
+    1,
+    output_names,
+    1
+  );
+
+  cv::Mat raw_boxes = getYoloBox(output);
+  std::vector<YoloBoundingBox> filtered_boxes = ProcessYoloOutputs(raw_boxes, image.size());
+  for (auto &box: filtered_boxes) {
+    cv::rectangle(image, box.bounding_box, cv::Scalar(0, 255, 0), 2);
+  }
+  cv::imshow("Test Image", image);
+  cv::waitKey(0);
   return 0;
 }
